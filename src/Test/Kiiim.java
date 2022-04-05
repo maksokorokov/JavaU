17273ffe1f3c113dbff0fabd6268d2c311d0d4e3
Maxim Okorokov <okoro99@mail.ru>
2022-04-05 16:49:51 +0300

    Init

---
+++ src/Test/Kiiim.java
@@ -0,0 +1,7 @@
+package Test;
+
+public class Kiiim {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Test/Test.java
+++ src/Test/Test.java
@@ -3,5 +3,6 @@ package Test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Max!!!");
+        System.out.println("mmm");
     }
 }
