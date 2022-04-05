59e8429bd5f6b44b759acc34b67a0f3176b07d7e
Maxim Okorokov <okoro99@mail.ru>
2022-04-05 16:55:24 +0300

    Init 1

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -4,5 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Max!!!");
         System.out.println("mmm");
+        System.out.println("mmm");
+        System.out.println("mmm");
     }
 }
