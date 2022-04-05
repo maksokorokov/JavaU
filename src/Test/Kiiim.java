947fb4d310e955c361d34c25c78164e45b68d027
Maxim Okorokov <okoro99@mail.ru>
2022-04-05 18:58:14 +0300

    clon1

--- src/Test/Kiiim.java
+++ src/Test/Kiiim.java
@@ -3,6 +3,8 @@ package Test;
 public class Kiiim {
     public static void main(String[] args) {
         System.out.println("posle clona");
+        System.out.println("posle clona");
+        System.out.println("posle clona");
 
     }
 }
